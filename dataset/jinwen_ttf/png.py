b0f2533a2bc6a60d9d877796dad110837fc2194a
dihch <dihch@shanghaitech.edu.cn>
2022-12-31 18:01:18 +0800

    ttf to png

---
+++ dataset/jinwen_ttf/png.py
@@ -0,0 +1,11 @@
+from fontTools.ttLib import TTFont
+from fontTools.pens.freetypePen import FreeTypePen
+from fontTools.misc.transform import Offset
+
+pen = FreeTypePen(None) # 实例化Pen子类
+font = TTFont("dataset\jinwen_ttf\jinwen.ttf") # 实例化TTFont
+glyph = font.getGlyphSet()["uni9C9B"] # 通过字形名称选择某一字形对象
+glyph.draw(pen) # “画”出字形轮廓
+width, ascender, descender = glyph.width, font['OS/2'].usWinAscent, -font['OS/2'].usWinDescent # 获取字形的宽度和上沿以及下沿
+height = ascender - descender # 利用上沿和下沿计算字形高度
+pen.show(width=width, height=height, transform=Offset(0, -descender)) # 显示以及矫正
---
+++ dataset/jinwen_ttf/ttf.py
@@ -0,0 +1,17 @@
+from fontTools.ttLib import TTFont
+
+def get_char_list_from_ttf(font_file):
+    ' 给定font_file,获取它的中文字符 '
+    f_obj = TTFont(font_file)
+    m_dict = f_obj.getBestCmap()
+    
+    unicode_list = []
+    for key, _ in m_dict.items():
+        unicode_list.append(key)
+        
+    char_list = [chr(ch_unicode) for ch_unicode in unicode_list]
+    return char_list
+ 
+font_file = "dataset\jinwen_ttf\jinwen.ttf"
+chars = get_char_list_from_ttf(font_file)
+print(chars)
