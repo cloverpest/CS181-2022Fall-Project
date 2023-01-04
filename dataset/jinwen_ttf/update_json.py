2bc050dd478201bdf8a4bb5daba3521e17dbdbdc
qinwch <qinwch@shanghaitech.edu.cn>
2023-01-04 23:23:17 +0800

    poetry_three_front_part

---
+++ dataset/jinwen_ttf/update_json.py
@@ -0,0 +1,14 @@
+import json
+def update_json(old, new):
+    old_dict = {}
+    with open(old, 'r', encoding='utf-8') as fp:
+        old_dict = json.load(fp)
+        with open(new, 'r', encoding='utf-8') as fp2:
+            new_dict = json.load(fp2)
+            for key in new_dict.keys():
+                    old_dict[key] += (new_dict[key] + 1)
+    with open(old, 'w', encoding='utf-8') as fp3:
+        json.dump(old_dict, fp3, ensure_ascii=False, indent=0)
+
+update_json("poetry.json", "poetry_0_1250.json")
+update_json("poetry.json", "poetry_4000_4700.json")
